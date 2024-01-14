d5c1a496979607d3225be228e9cccdd2b423f66e
ZisElite <zisispsalidas@gmail.com>
2024-01-15 01:19:12 +0200

    design document and more ui

--- godot/Scripts/new.gd
+++ godot/Scripts/new.gd
@@ -1,7 +1,6 @@
 extends Button
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pressed.connect(self._new_library)
 
---
+++ godot/Scripts/open.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+
+func _ready():
+	pressed.connect(self._open_library)
+
+
+
+func _open_library():
+	pass
