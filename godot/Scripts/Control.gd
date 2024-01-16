f55d1cd1d5998d6a2447d28021c60672f167ee98
ZisElite <zisispsalidas@gmail.com>
2024-01-16 17:52:26 +0200

    almost finished library view

--- godot/Scripts/Control.gd
+++ godot/Scripts/Control.gd
@@ -3,26 +3,37 @@ extends Control
 var main
 var new_lib
 var open_lib
+var view_lib
 
 func _ready():
 	main = get_node("main menu")
 	new_lib = get_node("new library")
 	open_lib = get_node("open library")
+	view_lib = get_node("library view")
 
 func _main_menu():
 	main.visible = true
 	new_lib.visible = false
 	open_lib.visible = false
+	view_lib.visible = false
 
 func _new_library():
 	main.visible = false
 	new_lib.visible = true
 	open_lib.visible = false
+	view_lib.visible = false
 	
 func _open_library():
 	main.visible = false
 	new_lib.visible = false
 	open_lib.visible = true
+	view_lib.visible = false
+
+func _view_library():
+	main.visible = false
+	new_lib.visible = false
+	open_lib.visible = false
+	view_lib.visible = true
 
 func _input(event):
 	if event is InputEventKey and OS.get_keycode_string(event.keycode) == "Escape":
--- godot/Scripts/open library_manager.gd
+++ godot/Scripts/open library_manager.gd
@@ -7,5 +7,5 @@ var cancel
 func _ready():
 	confirm = get_node("ScrollContainer/HBoxContainer/VBoxContainer/HBoxContainer/confirm")
 	cancel = get_node("ScrollContainer/HBoxContainer/VBoxContainer/HBoxContainer/cancel")
-	#confirm.pressed.connect()
+	confirm.pressed.connect(get_parent()._view_library)
 	cancel.pressed.connect(get_parent()._main_menu)
